2f104e3b6a237395d0e4d7167a167a505ed928a8
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-07-16 20:32:31 -0500

    no powerpath

--- src/lib/clients.ts
+++ src/lib/clients.ts
@@ -1,6 +1,5 @@
 import { env } from "@/env"
 import * as onerosterInternal from "./oneroster"
-import * as powerpathInternal from "./powerpath"
 import * as qtiInternal from "./qti"
 
 /**
@@ -11,19 +10,6 @@ import * as qtiInternal from "./qti"
  */
 export const oneroster = new onerosterInternal.Client({
 	serverUrl: env.TIMEBACK_ONEROSTER_SERVER_URL,
-	tokenUrl: env.TIMEBACK_TOKEN_URL,
-	clientId: env.TIMEBACK_CLIENT_ID,
-	clientSecret: env.TIMEBACK_CLIENT_SECRET
-})
-
-/**
- * A singleton instance of the PowerPath API client.
- * This client is configured once with environment variables and should be used
- * for all PowerPath API interactions to ensure consistent configuration and
- * efficient token management.
- */
-export const powerpath = new powerpathInternal.Client({
-	serverUrl: env.TIMEBACK_POWERPATH_SERVER_URL,
 	tokenUrl: env.TIMEBACK_TOKEN_URL,
 	clientId: env.TIMEBACK_CLIENT_ID,
 	clientSecret: env.TIMEBACK_CLIENT_SECRET
