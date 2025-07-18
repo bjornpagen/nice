71b278ea40a57dc6ded33dbc7ceaea77c45315ac
Aiden Zepp <207584745+supersterling@users.noreply.github.com>
2025-07-18 13:00:42 -0500

    sidebar

---
+++ src/app/(user)/[subject]/[course]/[unit]/[lesson]/components/course-sidebar-course-materials.tsx
@@ -0,0 +1,159 @@
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
+import { LearningContentIcon } from "@/components/overview/learning-content-icons"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { cn } from "@/lib/utils"
+import type { CourseMaterial, LessonResource } from "@/lib/v2/types"
+import courseChallengeIllustration from "./images/course-challenge-sidebar-illustration.png"
+import quizIllustration from "./images/quiz-sidebar-illustration.png"
+import unitTestIllustration from "./images/unit-test-sidebar-illustration.png"
+
+export function CourseSidebarCourseMaterials({
+	index,
+	materials,
+	pathname
+}: {
+	index: number
+	materials: CourseMaterial[]
+	pathname: string
+}) {
+	const material = materials[index]
+	if (material == null) {
+		return undefined
+	}
+
+	return (
+		<div id="course-sidebar-course-materials transition-all">
+			<ScrollArea className="h-full">
+				<div className="flex flex-col divide-y divide-gray-200">
+					<MaterialItem material={material} pathname={pathname} />
+				</div>
+			</ScrollArea>
+		</div>
+	)
+}
+
+function MaterialItem({ material, pathname }: { material: CourseMaterial; pathname: string }) {
+	switch (material.type) {
+		case "Lesson":
+			return (
+				<React.Fragment>
+					{material.resources.map((resource) => (
+						<Link key={resource.path} href={resource.path}>
+							<div
+								className={cn(
+									"flex items-center gap-4 py-4 px-4 hover:bg-blue-100",
+									pathname === resource.path && "bg-blue-100 border-l-4 border-l-blue-600"
+								)}
+							>
+								<ResourceItemIcon key={resource.path} resource={resource} pathname={pathname} />
+								<div className="flex flex-col">
+									<span className={cn("text-sm text-gray-800", pathname === resource.path && "text-blue-800")}>
+										{resource.title}
+									</span>
+									{resource.type === "Exercise" && <span className="text-xs text-gray-500">Not started</span>}
+								</div>
+							</div>
+						</Link>
+					))}
+				</React.Fragment>
+			)
+		case "Quiz":
+			return (
+				<React.Fragment>
+					<Link href={material.path}>
+						<div
+							className={cn(
+								"flex items-center gap-4 py-4 px-4 hover:bg-blue-100",
+								pathname === material.path && "bg-blue-100 border-l-4 border-l-blue-600"
+							)}
+						>
+							<div className="flex flex-col">
+								<span className={cn("text-sm text-gray-800", pathname === material.path && "text-blue-800")}>
+									{material.title}
+								</span>
+								<span className="text-xs text-gray-500">Not started</span>
+							</div>
+						</div>
+					</Link>
+					<Image
+						src={quizIllustration}
+						alt="Quiz illustration"
+						width={400}
+						height={400}
+						className="w-full aspect-square object-contain"
+					/>
+				</React.Fragment>
+			)
+		case "UnitTest":
+			return (
+				<React.Fragment>
+					<Link href={material.path}>
+						<div
+							className={cn(
+								"flex items-center gap-4 py-4 px-4 hover:bg-blue-100",
+								pathname === material.path && "bg-blue-100 border-l-4 border-l-blue-600"
+							)}
+						>
+							<div className="flex flex-col">
+								<span className={cn("text-sm text-gray-800", pathname === material.path && "text-blue-800")}>
+									{material.title}
+								</span>
+								<span className="text-xs text-gray-500">Not started</span>
+							</div>
+						</div>
+					</Link>
+					<Image
+						src={unitTestIllustration}
+						alt="Unit test illustration"
+						width={400}
+						height={400}
+						className="w-full aspect-square object-contain"
+					/>
+				</React.Fragment>
+			)
+		case "CourseChallenge":
+			return (
+				<React.Fragment>
+					<Link href={material.path}>
+						<div
+							className={cn(
+								"flex items-center gap-4 py-4 px-4 hover:bg-blue-100",
+								pathname === material.path && "bg-blue-100 border-l-4 border-l-blue-600"
+							)}
+						>
+							<div className="flex flex-col">
+								<span className={cn("text-sm text-gray-800", pathname === material.path && "text-blue-800")}>
+									{material.title}
+								</span>
+								<span className="text-xs text-gray-500">Not started</span>
+							</div>
+						</div>
+					</Link>
+					<Image
+						src={courseChallengeIllustration}
+						alt="Course challenge illustration"
+						width={400}
+						height={400}
+						className="w-full aspect-square object-contain"
+					/>
+				</React.Fragment>
+			)
+		default:
+			return undefined
+	}
+}
+
+function ResourceItemIcon({ resource, pathname }: { resource: LessonResource; pathname: string }) {
+	switch (resource.type) {
+		case "Article":
+			return <LearningContentIcon variant="article" className={cn(pathname === resource.path && "bg-blue-100")} />
+		case "Exercise":
+			return <LearningContentIcon variant="exercise" className={cn(pathname === resource.path && "bg-blue-100")} />
+		case "Video":
+			return <LearningContentIcon variant="video" className={cn(pathname === resource.path && "bg-blue-100")} />
+		default:
+			return undefined
+	}
+}
---
+++ src/app/(user)/[subject]/[course]/[unit]/[lesson]/components/course-sidebar.tsx
@@ -0,0 +1,110 @@
+"use client"
+
+import * as errors from "@superbuilders/errors"
+import _ from "lodash"
+import Image from "next/image"
+import { notFound, usePathname } from "next/navigation"
+import * as React from "react"
+import { useSidebar } from "@/components/ui/sidebar"
+import { cn } from "@/lib/utils"
+import { type Course, getCourseMaterials } from "@/lib/v2/types"
+import { CourseSidebarCourseBreadcrumbs } from "./course-sidebar-course-breadcrumbs"
+import { CourseSidebarCourseCarousel } from "./course-sidebar-course-carousel"
+import { CourseSidebarCourseMaterials } from "./course-sidebar-course-materials"
+import courseIconForeground from "./images/7a86b575f4360619-course-icon.svg"
+import courseIconBackground from "./images/course-accordion-bg.png"
+
+export function CourseSidebar({
+	coursePromise,
+	className
+}: {
+	coursePromise: Promise<Course | undefined>
+	className?: string
+}) {
+	const pathname = usePathname()
+	const { open } = useSidebar()
+
+	const course = React.use(coursePromise)
+	if (course == null) {
+		notFound()
+	}
+
+	const materials = getCourseMaterials(course)
+	if (materials.length === 0) {
+		throw errors.new(`course sidebar: no content found for course: ${course.title}`)
+	}
+
+	const cursor = _.findIndex(materials, (material) => {
+		if (material.type === "Lesson") {
+			return _.some(material.resources, (resource) => resource.path === pathname)
+		}
+		return material.path === pathname
+	})
+	if (cursor === -1) {
+		throw errors.new(`course sidebar: material not found: ${pathname}`)
+	}
+
+	const material = materials[cursor]
+	if (material == null) {
+		throw errors.new(`course sidebar: material not found: ${pathname}`)
+	}
+
+	const [index, setIndex] = React.useState<number>(_.clamp(cursor, 0, materials.length - 1))
+
+	return (
+		<div
+			id="course-sidebar"
+			className={cn("bg-gray-100 border-r border-gray-200 h-full p-4", className, !open && "hidden")}
+		>
+			<div className="px-6 pb-4 flex-1 overflow-hidden bg-white rounded-md shadow-md">
+				<div id="course-sidebar-course-title" className="text-lg font-bold mt-4">
+					<div className="flex items-center gap-4">
+						<div className="w-10 h-10 rounded flex items-center justify-center relative">
+							<Image src={courseIconBackground} alt="Course icon background" fill className="object-cover rounded" />
+							<Image
+								src={courseIconForeground}
+								alt="Course icon foreground"
+								className="w-6 h-6 relative z-10 brightness-0 invert"
+							/>
+						</div>
+
+						<span className="font-medium text-gray-900 text-xl capitalize">{course.title}</span>
+					</div>
+				</div>
+
+				{/* Separator */}
+				<div className="h-px my-4 bg-gray-200" />
+
+				<div className="mt-4">
+					<CourseSidebarCourseCarousel course={course} materials={materials} index={index} setIndex={setIndex} />
+				</div>
+
+				{/* Separator */}
+				<div className="h-px my-4 bg-gray-200" />
+
+				<CourseSidebarCourseMaterials index={index} materials={materials} pathname={pathname} />
+
+				{/* Separator */}
+				<div className="h-px my-4 bg-gray-200" />
+
+				<CourseSidebarCourseBreadcrumbs
+					course={_.pick(course, ["path", "title"])}
+					material={material}
+					pathname={pathname}
+				/>
+
+				<div id="course-sidebar-footer" className="p-2 my-4 flex flex-col gap-2 text-center">
+					<div className="text-xs text-gray-600">© 2025 Nice Academy</div>
+					<div className="text-xs text-gray-500 flex flex-wrap gap-x-4 gap-y-1 justify-center">
+						<span className="hover:text-gray-700 underline cursor-not-allowed whitespace-nowrap">Terms of use</span>
+						<span className="hover:text-gray-700 underline cursor-not-allowed whitespace-nowrap">Privacy Policy</span>
+						<span className="hover:text-gray-700 underline cursor-not-allowed whitespace-nowrap">Cookie Notice</span>
+						<span className="hover:text-gray-700 underline cursor-not-allowed whitespace-nowrap">
+							Accessibility Statement
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
