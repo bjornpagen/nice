bb550dd6ff20267434b69c3ded61f24a301bb510
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-08-13 11:27:02 -0500

    move toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 						</UserSyncProvider>
 					</ClerkProvider>
 				</React.Suspense>
-				<Toaster />
+				<Toaster position="top-right" />
 			</body>
 		</html>
 	)
