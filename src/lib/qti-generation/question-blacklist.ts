3de6997eee7770aae405da5ef8363ffd45b9e343
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-08-16 00:25:52 -0500

    Add new question ID to the blacklist
    - Added "x0630ce7689f9aec4" to the `QuestionIdBlacklist` in `question-blacklist.ts` to prevent processing of an additional specific question.

--- src/lib/qti-generation/question-blacklist.ts
+++ src/lib/qti-generation/question-blacklist.ts
@@ -15,7 +15,8 @@ export const QuestionIdBlacklist: ReadonlySet<string> = new Set<string>([
 	"xa7f40f16422ca17d",
 	"x76f53e7969c909b4",
 	"xe484b6ee5e0adf27",
-	"x2759ec290a99458d"
+	"x2759ec290a99458d",
+	"x0630ce7689f9aec4"
 ])
 
 export function isQuestionIdBlacklisted(questionId: string): boolean {
