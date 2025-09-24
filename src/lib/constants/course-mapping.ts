bf1d53f4e4f603b03d9e64d17d0eca6b81178591
ameeralns <ameeralns35@gmail.com>
2025-09-23 19:22:31 -0500

    Comment out unused course IDs in course-mapping constants for clarity and maintainability

--- src/lib/constants/course-mapping.ts
+++ src/lib/constants/course-mapping.ts
@@ -78,12 +78,12 @@ export const ALL_HARDCODED_COURSE_IDS: CourseId[] = [
 	"x3e2fc37246974751", // ap-college-us-government-and-politics
 	"x2d2f703b37b450a3", // AP®︎/College Computer Science Principles
 	"x435b1de09a877dd7", // 8th grade reading and vocab
-	"x7538838f96af3430", //7th grade reading and vocab
+	// "x7538838f96af3430", //7th grade reading and vocab
 	"x8ddea1200317e822", //6th grade reading and vocab
 	"xaf0c1b5d7010608e", //3rd grade reading and vocab
 	"xb350e60168d6e96f", //5th grade reading and vocab
-	"xd45453bfd2ae8614", //9th grade reading and vocab
-	"xd89aabc5d5847a2b", //10th grade reading and vocab
+	// "xd45453bfd2ae8614", //9th grade reading and vocab
+	// "xd89aabc5d5847a2b", //10th grade reading and vocab
 	"xe0e52cf20ce2546d", //4th grade reading and vocab
 	"xfb4fc0bf01437792" //2nd grade reading and vocab
 ]
@@ -133,12 +133,12 @@ export const HARDCODED_HISTORY_COURSE_IDS = [
 	// "x2d2f703b37b450a3", //AP®︎/College Computer Science Principles
 
 	"x435b1de09a877dd7", // 8th grade reading and vocab
-	"x7538838f96af3430", //7th grade reading and vocab
+	// "x7538838f96af3430", //7th grade reading and vocab
 	"x8ddea1200317e822", //6th grade reading and vocab
 	"xaf0c1b5d7010608e", //3rd grade reading and vocab
 	"xb350e60168d6e96f", //5th grade reading and vocab
-	"xd45453bfd2ae8614", //9th grade reading and vocab
-	"xd89aabc5d5847a2b", //10th grade reading and vocab
+	// "xd45453bfd2ae8614", //9th grade reading and vocab
+	// "xd89aabc5d5847a2b", //10th grade reading and vocab
 	"xe0e52cf20ce2546d", //4th grade reading and vocab
 	"xfb4fc0bf01437792" //2nd grade reading and vocab
 ] as const
